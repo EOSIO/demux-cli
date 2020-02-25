3c25545c76e239e785bff69a6d03a86bbb89a23e
Jon La Marr <jon.lamarr-ext@block.one>
2020-02-25 17:20:11 -0500

    Bump typescript version

--- src/generators/init/dependencies.js
+++ src/generators/init/dependencies.js
@@ -5,7 +5,7 @@ const dependencies = {
   demuxPostgres: 'demux-postgres@4.0.1',
   massive: 'massive@5.11.2',
   // Dev Dependencies
-  typescript: 'typescript@3.3.3',
+  typescript: 'typescript@3.8.2',
   typesMassive: '@types/massive@5.4.3',
   typesBunyan: '@types/bunyan@1.8.6'
 }
